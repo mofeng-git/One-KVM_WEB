1cc1024c4aec6afb5f46403572bf608fbb4fb684
mofeng-git <mofeng654321@hotmail.com>
2024-06-10 21:05:34 +0800

    添加统计

---
+++ docs/js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2ef192ca9c0c5ddebe75dbd2c468dd53";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
